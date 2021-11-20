6b1393c2cdc2520e47dfd8869014fe5a5b4da361
jaydavis359 <jaydavis_1@hotmail.com>
2021-11-20 21:20:58 +0000

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn molten_java.wsgi:application
--- molten_java/settings.py
+++ molten_java/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wbhsl&ah9d)@@6!-n@ewo*7xd$a=rwj1(^5ds7-(dtzr!2dhv+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jay359-molten-java.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'molten_java.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABESES_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
