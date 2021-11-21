b1fa1d3589d985651b575f65e011cf52fe37c123
jaydavis359 <jaydavis_1@hotmail.com>
2021-11-21 00:46:07 +0000

    heroku debugging

--- molten_java/settings.py
+++ molten_java/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'molten_java.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABESES_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
