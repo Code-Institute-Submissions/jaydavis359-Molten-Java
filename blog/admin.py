4c76f7685b61f4e4fddfc1e66382aae6628dcfbf
jaydavis359 <jaydavis_1@hotmail.com>
2021-12-08 13:07:37 +0000

    Add blog comments to admin for edit/delete functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from .models import Post
+from .models import Comment
 
 admin.site.register(Post)
+
+admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "name", "email", "body", "date_added")
+    search_fields = ("name", "email", "post")
